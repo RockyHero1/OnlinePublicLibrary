236b73355be4d70987546d9fdaf0f32e8401a5ab
RockyHero1 <roshangjangid21@gmail.com>
2023-07-10 15:04:54 +0530

    roshan/mansi: add spirngboot setup

---
+++ onlinePublicLibrary/src/main/java/com/onlinePublicLibrary/onlinePublicLibrary/OnlinePublicLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.onlinePublicLibrary.onlinePublicLibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinePublicLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinePublicLibraryApplication.class, args);
+	}
+
+}
---
+++ onlinePublicLibrary/src/test/java/com/onlinePublicLibrary/onlinePublicLibrary/OnlinePublicLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onlinePublicLibrary.onlinePublicLibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinePublicLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
